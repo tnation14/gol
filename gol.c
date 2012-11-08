173cacb41fa2556ab17dd25dd410d121066355d7
tnation14 <tnation14@gmail.com>
2012-11-08 18:09:30 -0500

    Working neighbor function 	modified:   gol.c

--- gol.c
+++ gol.c
@@ -7,7 +7,6 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <string.h>
-//remove me.
 
 // NOTE: Our code must pass, at minimum, the following criteria:
   // TODO: Error check EVERYTHING that returns a value! (Yeah, everything)
@@ -95,28 +94,27 @@ int numNeighbors(int xcoord, int ycoord, int rows, int cols, char *board, int nu
   */
   x = xcoord;
   y = ycoord;
-  
-  for(i = 0; i < 2* numcoords-1; i +=2){
-  	for(j = -1; i< 2; k++){
-  	int currentrow;
-  	currentrow = x+j;
-  	if(currentrow == rows){
-  	  currentrow = 0;	  
-  	}
-  	for(k = -1;j<2;j++){
-  	  int currentcol;
-  	  currentcol = y +k;
-  	  if(currentcol == cols){
-  	  	currentcol = 0;
-  	  }  	  
-  	  if(currentrow == x && currentcol == y){
-  	    continue;
-  	  }else if(board[currentrow*rows+currentcol] == '!'){
-  	    neighborcounter++;
-  	  }
+  neighborcounter = 0;
+  	for(j = -1; j< 2; j++){
+  		int currentrow;
+  		currentrow = x+j;
+  		if(currentrow == rows){
+  	  	  currentrow = 0;  	  
+  		}
+  	  for(k = -1;k<2;k++){
+  	    int currentcol;
+  	    currentcol = y +k;
+  	    if(currentcol == cols){
+  	    	currentcol = 0;
+  	    }  	  
+  	    if(currentrow == x && currentcol == y){
+  	      continue;
+  	    }else if(board[currentrow*rows+currentcol] == '!'){
+  	  	  printf("Neighbor: (%d,%d)\n", currentrow, currentcol);
+  	      neighborcounter++;
+  	    }
 
   	}
-  }
   }
   
 
@@ -138,17 +136,40 @@ int main(int argc, char *argv[]) {
   }
     
   // TODO: Keep this around while I test something else
-  int rows,cols,iters,numCoords,counter,x,y,i,j;
+  int rows,cols,iters,numCoords,counter,x,y;
   fscanf(inFile, "%d %d %d %d", &rows, &cols, &iters, &numCoords);
   
   // Create game board
   char *board = NULL;
   board = make2DArray(rows,cols,inFile,numCoords);
   print(board,atoi(argv[2]),rows,cols);
-  
+  int* coords=NULL;
+  coords = (int *)malloc(8*sizeof(int));
+  if (coords == NULL) {
+    printf("malloc failed");
+    exit(1);
+  } 
+  counter = 0;
+  int index;
+  index = 0;
+  fseek(inFile, 0, SEEK_SET);
+  fscanf(inFile, "%d %d %d %d", &rows, &cols, &iters, &numCoords);
+
+  while(counter < numCoords){
+  	fscanf(inFile, "%d %d", &coords[index],&coords[index+1]);	
+  	index +=2;
+  	counter ++;
+  } 
+  for(counter = 0; counter <2*numCoords-1; counter+=2){
+  	int neighbors;
+  	printf("(%d,%d)\n",coords[counter], coords[counter+1]);
+  	neighbors = numNeighbors(coords[counter],coords[counter+1],rows,cols,board,numCoords);
+  	
+  }
  
   
   free(board);
+  free(coords);
   fclose(inFile);
  
   return 0;
